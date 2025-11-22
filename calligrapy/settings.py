3195952b717afbd1d7d619035fb8ede4a941395e
LokeshShrestha <lokesh591shrestha@gmail.com>
2025-11-22 09:34:21 +0545

    bug

--- calligrapy/settings.py
+++ calligrapy/settings.py
@@ -183,14 +183,10 @@ SIMPLE_JWT = {
 }
 
 # CORS Configuration
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOWED_ORIGINS = os.getenv(
+    'CORS_ALLOWED_ORIGINS',
+    "http://localhost:8080,http://localhost:5173,http://localhost:3000,http://127.0.0.1:8080,http://127.0.0.1:5173,http://127.0.0.1:3000"
+).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
 
