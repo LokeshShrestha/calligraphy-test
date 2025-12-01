fd8b48bc937bd36dd8550423177106fa9dfa467e
LokeshShrestha <lokesh591shrestha@gmail.com>
2025-12-01 18:40:10 +0545

    changes

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser from environment variables (if set)
+python manage.py createsuperuser_env || true
